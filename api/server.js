58289df266f707bf8ad9de153343cc690c08ef01
Vincent Sanders <vincesanders@hotmail.com>
2020-02-25 13:12:45 -0600

    Authorization endpoints working with cookies.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const connectSessionKnexStore = require('connect-session-knex')(session);
+const knex = require('../database/dbConfig');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -10,6 +13,25 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use(session({
+  name: 'nameofcookie',
+  secret: 'keep it secret, keep it safe',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, //true in production
+    httpOnly: true //javascript code can't access
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new connectSessionKnexStore({
+    knex: knex,
+    tablename: 'sessions',
+    createtable: true,
+    sidfieldname: 'sid',
+    clearInterval: 1000 * 60 * 15
+  })
+}));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,6 +11,7 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
+      req.session.user = user;
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -25,6 +26,7 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
@@ -35,6 +37,20 @@ router.post('/login', (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ message: "Unable to logout." });
+      } else {
+        res.status(200).json({ message: 'Logout successful.' });
+      }
+    });
+  } else {
+    res.status(500).json({ message: 'You are already logged out.' });
+  }
 });
 
 module.exports = router;
